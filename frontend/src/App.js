01d430e5b1306a6aca26ad01ae4d0508ade52809
Kelvin <laikelvin8@gmail.com>
2022-01-01 17:14:03 +0000

    Tested fetch request

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const requestOptions = {
+    headers: {'Content-Type': 'application/json'}
+  }
+
+  fetch('/api/message',requestOptions)
+  .then(response => response.json())
+  
+  .then(data => {
+    console.log(data)
+  })
+
   return (
     <div className="App">
-   <h1>Hello world</h1>
+      <h1>Hello world</h1>
     </div>
   );
 }
