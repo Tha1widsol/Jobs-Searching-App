8dc79575a293053506940fb759a5a6537a6519fc
Kelvin <laikelvin8@gmail.com>
2022-01-08 21:28:37 +0000

    Added dynamic 'option' endpoint in path

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -3,7 +3,7 @@ from knox import views as knox_views
 from . import views
 
 urlpatterns = [
-  path('auth/register',views.RegisterAPI.as_view(),name = 'register'),
+  path('auth/register/<str:option>',views.RegisterAPI.as_view(),name = 'register'),
   path('auth/login',views.LoginAPI.as_view(),name = 'login'),
   path('auth/logout',knox_views.LogoutView.as_view(),name = 'knox_logout')
 ]
