5efa31cbcc464c903c4a76f4e5149b295c52b38b
Kelvin <laikelvin8@gmail.com>
2022-01-08 21:27:24 +0000

    Removed testing API fetch request

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,6 @@ import Navbar from './components/Global/Navbar'
 import PagesRoutes from './components/Global/PagesRoutes';
 
 function App() {
-  const requestOptions = {
-    headers: {'Content-Type': 'application/json'}
-  }
-
-  fetch('/api/message',requestOptions)
-  .then(response => response.json())
-  
-  .then(data => {
-    console.log(data)
-  })
-
   return (
     <div>
       <Navbar/>
