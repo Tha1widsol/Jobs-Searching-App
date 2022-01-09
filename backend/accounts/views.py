5187524bb5b34051c929080a01b50ca73a087027
Kelvin <laikelvin8@gmail.com>
2022-01-09 21:39:18 +0000

    Imported main.serializers file

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from rest_framework.response import Response
 from .serializers import *
+from main.serializers import UserSerializer
 from knox.models import AuthToken
 
 # Create your views here.
