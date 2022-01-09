9b8d94172e0b7fd0056da24ed63a6ebe5ffcf5ee
Kelvin <laikelvin8@gmail.com>
2022-01-09 21:40:44 +0000

    Added fetch currentUser useEffect function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,47 @@
+import React,{useEffect} from 'react'
 import Navbar from './components/Global/Navbar'
 import PagesRoutes from './components/Global/PagesRoutes';
+import {useAppDispatch} from './components/Global/features/hooks'
+import {login,logout} from './components/Global/features/user'
+import axios from 'axios'
 
 function App() {
+  const dispatch = useAppDispatch()
+  const token = localStorage.getItem('token')
+  
+  
+  useEffect(() => {
+    if(!token) return
+
+     axios.get('/api/currentUser',{
+        headers: {
+            Authorization:`Token ${token}`
+        }
+    })
+    
+    .then(response => {
+        const data = response.data
+        if (data.email){
+            dispatch(
+                login({
+                    id: data.id,
+                    email: data.email,
+                    is_an_employer: data.is_an_employer,
+                    logged_in: true
+                })
+            )
+          
+        }
+
+        else dispatch(logout())
+    })
+
+    .catch(error => {
+        console.log(error)
+    })
+
+},[dispatch,token])
+
   return (
     <div>
       <Navbar/>
