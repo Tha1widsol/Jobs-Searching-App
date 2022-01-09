6d45ca85d29bb107d440435a94bd10e19d9eb08a
Kelvin <laikelvin8@gmail.com>
2022-01-09 21:39:00 +0000

    Removed UserSerializer class

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -2,11 +2,6 @@ from rest_framework import serializers
 from .models import User
 from django.contrib.auth import authenticate
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = '__all__'
-
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
