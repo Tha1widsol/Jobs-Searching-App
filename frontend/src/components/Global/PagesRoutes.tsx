4b82db0f679449a912bfe319925d2018338474c7
Kelvin <laikelvin8@gmail.com>
2022-01-09 21:42:14 +0000

    Added LoginPage component route

--- frontend/src/components/Global/PagesRoutes.tsx
+++ frontend/src/components/Global/PagesRoutes.tsx
@@ -2,16 +2,18 @@ import React from 'react'
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
 import HomePage from '../HomePage/HomePage'
 import RegisterPage from '../RegisterPage/RegisterPage'
+import LoginPage from '../LoginPage/LoginPage'
 
 export default function PagesRoutes() {
     return (
         <div>
             <Router>
                 <Routes>
-                    <Route path = '/' element={<HomePage/>}></Route>
-                    <Route path = '/home' element={<HomePage/>}></Route>
-                    <Route path = '/register/job_seeker' element={<RegisterPage/>}></Route>
-                    <Route path = '/register/employer' element={<RegisterPage/>}></Route>
+                    <Route path = '/' element = {<HomePage/>}/>
+                    <Route path = '/home' element = {<HomePage/>}/>
+                    <Route path = '/register/job_seeker' element = {<RegisterPage/>}/>
+                    <Route path = '/register/employer' element = {<RegisterPage/>}/>
+                    <Route path = '/login' element = {<LoginPage/>}/>
                 </Routes>
             </Router>
         </div>
