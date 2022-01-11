25fc1763a7d92380877505726e44414f8b98db2c
Kelvin <laikelvin8@gmail.com>
2022-01-11 23:17:16 +0000

    Added dropdown reducer in reducer

--- frontend/src/components/Global/features/store.ts
+++ frontend/src/components/Global/features/store.ts
@@ -1,9 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit'
 import userReducer from './user'
+import dropdownReducer from './dropdown'
 
 export const store = configureStore({
   reducer: {
-    user: userReducer
+    user: userReducer,
+    dropdown: dropdownReducer
   }
 })
 
