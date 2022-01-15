84c6f5b43d8dbd7b6e8d05d5ca355001125cf0e2
Kelvin <laikelvin8@gmail.com>
2022-01-15 21:51:41 +0000

    Removed dropdownReducer

--- frontend/src/components/Global/features/store.ts
+++ frontend/src/components/Global/features/store.ts
@@ -1,11 +1,9 @@
 import {configureStore} from '@reduxjs/toolkit'
 import userReducer from './user'
-import dropdownReducer from './dropdown'
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
-    dropdown: dropdownReducer
   }
 })
 
