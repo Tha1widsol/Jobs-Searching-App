c91ba2151f81e26e8045873c2651bdbc64b7bb9a
Kelvin <laikelvin8@gmail.com>
2022-01-15 21:51:00 +0000

    Added about property in profile interface

--- frontend/src/components/CreateProfilePage/types/CreateProfileInterface.ts
+++ frontend/src/components/CreateProfilePage/types/CreateProfileInterface.ts
@@ -1,5 +1,6 @@
 export interface FormProps{
-    firstName: {isValid: boolean, msg: string},
-    middleName: {isValid: boolean, msg: string},
+    firstName: {isValid: boolean, msg: string}
+    middleName: {isValid: boolean, msg: string}
     lastName:  {isValid: boolean, msg: string}
+    about:  {isValid: boolean, msg: string}
 }
