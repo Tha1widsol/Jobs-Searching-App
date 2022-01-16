4b17baf2a42e199822659b8c156f4d2ba1e8f347
Kelvin <laikelvin8@gmail.com>
2022-01-16 17:39:02 +0000

    Cleaned up HomePage.tsx

--- frontend/src/components/HomePage/HomePage.tsx
+++ frontend/src/components/HomePage/HomePage.tsx
@@ -3,20 +3,20 @@ import './css/HomePage.css'
 
 export default function HomePage() {
     return (
-        <div>
+        <div style = {{textAlign:'center'}}>
             <h1 id = 'you_are'>You are...</h1>
 
-            <div id = 'home'>
-                <div className = 'home-container'>
-                    <h2 className = "home-heading"><u>Looking for a job:</u> </h2>
+            <div id = 'containers'>
+                <div className = 'container'>
+                    <h2 className = 'heading'><u>Looking for a job:</u></h2>
                     <p>Find your dream job right now with one of the most trusted job searching engines on the web.</p>
-                    <a href = '/register/job_seeker' ><button id = 'job_seeker' >Add your profile</button></a> 
+                    <a href = '/register/job_seeker' ><button id = 'jobseeker' >Add your profile</button></a> 
                 </div>
                 
-                <div className = 'vertical'></div>
+                <div id = 'vertical'></div>
 
-                <div className = 'home-container'>
-                    <h2 className = 'home-heading'><u>An employer:</u> </h2>
+                <div className = 'container'>
+                    <h2 className = 'heading'><u>An employer:</u></h2>
                     <p>Setup your company and start adding jobs using our system.</p>
                     <a href = '/register/employer' ><button id = 'company' >Add your company</button></a> 
                 </div>
