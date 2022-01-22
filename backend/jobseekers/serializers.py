adb3cc8456ffa3f5656224f7b1435f4ac84888f4
Kelvin <laikelvin8@gmail.com>
2022-01-22 22:54:39 +0000

    Added serializers.py in jobseekers

---
+++ backend/jobseekers/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from main.serializers import UserSerializer
+from .models import Profile
+from main.models import Skill
+from accounts.serializers import *
+
+class CreateProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        exclude = ['user','skills']
+
+class SkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = ['name']
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only = True)
+    skills = SkillSerializer(read_only = True, many = True)
+
+    class Meta:
+        model = Profile
+        fields = '__all__'
+    
