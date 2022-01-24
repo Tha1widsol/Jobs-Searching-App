a7308049693c9435951469baa34031931c225bc7
Kelvin <laikelvin8@gmail.com>
2022-01-24 19:47:13 +0000

    Added CheckNoProfileExists function

--- frontend/src/components/Global/ProtectedRoutes.tsx
+++ frontend/src/components/Global/ProtectedRoutes.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import {Navigate, Outlet} from 'react-router'
+import {Outlet,Navigate} from 'react-router'
+import { useNavigate } from 'react-router-dom';
 import {useAppSelector} from './features/hooks';
 import LoginPage from '../public/LoginPage/LoginPage';
+import PublicHomePage from '../public/PublicHomePage/PublicHomePage';
+import axios from 'axios'
 
 const User = () => {
   const user = useAppSelector(state => state.user.values)
@@ -15,4 +18,31 @@ export const CheckNotLoggedIn = () => {
 
 export const CheckLoggedIn = () => { 
     return User().loggedIn ? <Outlet/> : <LoginPage/>
+}
+
+export const CheckNoProfileExists = () => { 
+  const token = localStorage.getItem('token')
+  const navigate = useNavigate()
+
+  if (User().isAnEmployer) return <Navigate to = '/'/>
+
+  const requestOptions = { 
+    headers:{'Content-Type':'application/json', Authorization:`Token ${token}`}
+  }
+
+  axios.get('/api/profile',requestOptions)
+  .then(response => {
+       if (response.status == 200)
+          return navigate('/')
+  })
+  
+ .catch(error => {
+     if (error.response.status !== 404){
+      return navigate('/')
+     }
+
+ })
+
+ return <Outlet/>
+
 }
