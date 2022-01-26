ec14208e57af718074855f395c294bb15848e144
Kelvin <laikelvin8@gmail.com>
2022-01-26 21:20:55 +0000

    Added CheckHomePage function

--- frontend/src/components/Global/ProtectedRoutes.tsx
+++ frontend/src/components/Global/ProtectedRoutes.tsx
@@ -3,14 +3,14 @@ import {Outlet,Navigate} from 'react-router'
 import { useNavigate } from 'react-router-dom';
 import {useAppSelector} from './features/hooks';
 import LoginPage from '../public/LoginPage/LoginPage';
-import PublicHomePage from '../public/PublicHomePage/PublicHomePage';
+import JobSeekersHomePage from '../Jobseekers/JobSeekersHomePage/JobSeekersHomePage';
+import EmployersHomePage from '../Employers/EmployersHomePage/EmployersHomePage';
 import axios from 'axios'
 
 const User = () => {
   const user = useAppSelector(state => state.user.values)
   return user
 }
-
 
 export const CheckNotLoggedIn = () => {
   return User().loggedIn ? <Navigate to = '/'/> : <Outlet/> 
@@ -21,28 +21,33 @@ export const CheckLoggedIn = () => {
 }
 
 export const CheckNoProfileExists = () => { 
-  const token = localStorage.getItem('token')
   const navigate = useNavigate()
 
   if (User().isAnEmployer) return <Navigate to = '/'/>
 
+  const token = localStorage.getItem('token')
   const requestOptions = { 
     headers:{'Content-Type':'application/json', Authorization:`Token ${token}`}
   }
 
   axios.get('/api/profile',requestOptions)
   .then(response => {
-       if (response.status == 200)
-          return navigate('/')
+       if (response.status === 200)
+          navigate('/')
   })
   
  .catch(error => {
      if (error.response.status !== 404){
-      return navigate('/')
+      navigate('/')
      }
 
  })
 
  return <Outlet/>
 
+}
+
+export const CheckHomePage = () => {
+  const user = useAppSelector(state => state.user.values)
+  return User().loggedIn && !User().isAnEmployer ? <JobSeekersHomePage/> : User().loggedIn && User().isAnEmployer ? <EmployersHomePage/> : <Outlet/>
 }
