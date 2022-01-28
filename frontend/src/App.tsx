73e484046a0fd2451285e0d8cf57577efb18496f
Kelvin <laikelvin8@gmail.com>
2022-01-28 23:59:30 +0000

    dispatched fetchUser

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,45 +2,14 @@ import React,{useEffect} from 'react'
 import Navbar from './components/Global/Navbar/Navbar'
 import PagesRoutes from './components/Global/PagesRoutes';
 import {useAppDispatch} from './components/Global/features/hooks'
-import {login,logout} from './components/Global/features/user'
-import axios from 'axios'
+import {fetchUser} from './components/Global/features/user';
 
 function App() {
   const dispatch = useAppDispatch()
-  const token = localStorage.getItem('token')
 
   useEffect(() => {
-    if(!token || token === '') return
-
-     axios.get('/api/currentUser',{
-        headers: {
-            Authorization:`Token ${token}`
-        }
-    })
-    
-    .then(response => {
-        const data = response.data
-        
-        if (data.email){
-            dispatch(
-                login({
-                    id: data.id,
-                    email: data.email,
-                    isAnEmployer: data.is_an_employer,
-                    loggedIn: true
-                })
-            )
-          
-        }
-
-        else dispatch(logout())
-    })
-
-    .catch(error => {
-        console.log(error)
-    })
-
-},[token,dispatch])
+      dispatch(fetchUser())
+  },[])
 
   return (
     <div>
