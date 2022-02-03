5e4e8990c6f83564c993759c55d3740098ceee2a
Kelvin <laikelvin8@gmail.com>
2022-02-03 17:53:17 +0000

    Added toggleProfileStatus path

--- backend/jobseekers/urls.py
+++ backend/jobseekers/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('profile',views.ProfileAPI.as_view(),name = 'profile')
+    path('profile',views.ProfileAPI.as_view(),name = 'profile'),
+    path('toggleProfileStatus',views.ToggleProfileStatus.as_view(),name = 'toggleProfileStatus'),
 ]
