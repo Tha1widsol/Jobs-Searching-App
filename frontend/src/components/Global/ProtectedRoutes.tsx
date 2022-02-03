8fef59d3f61483f5817a366b702c7fa891f04431
Kelvin <laikelvin8@gmail.com>
2022-02-03 23:39:00 +0000

    Added CheckJobSeeker function

--- frontend/src/components/Global/ProtectedRoutes.tsx
+++ frontend/src/components/Global/ProtectedRoutes.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import {Outlet,Navigate} from 'react-router'
-import { useNavigate } from 'react-router-dom';
 import {useAppSelector} from './features/hooks';
 import LoginPage from '../public/LoginPage/LoginPage';
-import axios from 'axios'
 
 const isAnEmployer = localStorage.getItem('isAnEmployer')
 
@@ -15,37 +13,13 @@ export const CheckNotLoggedIn = () => {
   return User().isLoggedIn ? <Navigate to = '/'/> : <Outlet/> 
 }
 
-export const CheckLoggedIn = () => { 
-    return User().isLoggedIn ? <Outlet/> : <LoginPage/>
-}
-
-const GetProfile = () => {
-  let navigate = useNavigate()
-  const token = localStorage.getItem('token')
-  const requestOptions = { 
-    headers:{'Content-Type':'application/json', Authorization:`Token ${token}`}
-  }
-
-  axios.get('/api/profile',requestOptions)
-  .then(response => {
-       if (response.status === 200){
-         navigate('/')
-       }
-  })
-  
- .catch(error => {
-     if (error.response.status !== 404){
-      navigate('/')
-     }
-
- })
-
-}
-
-export const CheckNoProfileExists = () => { 
-  GetProfile()
+export const CheckJobSeeker = () => {
+  if (!User().isLoggedIn) return <Navigate to = '/login'/>
   if (isAnEmployer === 'true') return <Navigate to = '/'/>
-  
+
   return <Outlet/>
 }
 
+export const CheckLoggedIn = () => { 
+    return User().isLoggedIn ? <Outlet/> : <LoginPage/>
+}
