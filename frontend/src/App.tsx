fd504095761659f9312f221ac7cdef77f96c9d9c
Kelvin <laikelvin8@gmail.com>
2022-02-07 23:35:59 +0000

    Imported SuccessAlert component into App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import React,{useEffect} from 'react'
 import Navbar from './components/Global/Navbar/Navbar'
+import {useAppDispatch} from './components/Global/features/hooks';
 import PagesRoutes from './components/Global/PagesRoutes';
-import {useAppDispatch} from './components/Global/features/hooks'
 import {fetchUser} from './components/Global/features/user';
+import SuccessAlert from './components/Global/messages/SuccessAlert';
 
 function App() {
   const dispatch = useAppDispatch()
@@ -14,6 +15,7 @@ function App() {
   return (
     <div>
       <Navbar/>
+      <SuccessAlert/>
       <PagesRoutes/>
     </div>
     
