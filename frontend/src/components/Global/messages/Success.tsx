cc22aa07e49ede19a86d48e0917f6e651a2cc037
Kelvin <laikelvin8@gmail.com>
2022-02-07 23:37:33 +0000

    Deleted Success.tsx

--- frontend/src/components/Global/messages/Success.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Success({success} : {success : string}) {
-    return (
-        <div>
-            {success ? <li className='success'> {success}</li> : null}
-        </div>
-    )
-}
