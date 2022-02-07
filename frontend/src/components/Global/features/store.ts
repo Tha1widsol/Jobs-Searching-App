9d6c9acbdd44832e5216f1a2606f168a6dfdc0a5
Kelvin <laikelvin8@gmail.com>
2022-02-07 23:36:57 +0000

    Added successMsgReducer to store

--- frontend/src/components/Global/features/store.ts
+++ frontend/src/components/Global/features/store.ts
@@ -1,9 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit'
 import userReducer from './user'
+import successMsgReducer from './successMsg'
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    successMsg: successMsgReducer
   }
 })
 
