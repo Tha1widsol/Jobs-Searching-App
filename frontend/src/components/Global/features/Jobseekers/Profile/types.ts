064d7e3fdd930973a76e7f1b68be31421a61434e
Kelvin <laikelvin8@gmail.com>
2022-02-10 22:43:50 +0000

    Added profile types.ts

---
+++ frontend/src/components/Global/features/Jobseekers/Profile/types.ts
@@ -0,0 +1,21 @@
+export interface ProfileProps{
+    values: {
+        user: {email: string, isHired: boolean | null, isAnEmployer: boolean | null}
+        firstName: string
+        lastName: string
+        middleName?: string
+        skills: {name: string}[]
+        phone: string
+        logo?: string
+        cv: string
+        education: string
+        industry: string
+        distance: string
+        experience?: string
+        about: string
+        isActive: boolean | null
+        doesExist: boolean | null
+        isLoading: boolean
+    }
+  
+}
