e715ac1b552777cf7c776b5ff45d4669014b33f0
Kelvin <laikelvin8@gmail.com>
2022-02-10 22:41:51 +0000

    Added profileReducer to redux store

--- frontend/src/components/Global/features/store.ts
+++ frontend/src/components/Global/features/store.ts
@@ -1,11 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit'
 import userReducer from './user'
+import ProfileReducer from './Jobseekers/Profile/profile'
 import successMsgReducer from './successMsg'
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
-    successMsg: successMsgReducer
+    successMsg: successMsgReducer,
+    profile: ProfileReducer
   }
 })
 
