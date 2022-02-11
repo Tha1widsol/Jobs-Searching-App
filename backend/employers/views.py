ec2e81bae865c69fc502c94e69ddff7cd23f0c2e
Kelvin <laikelvin8@gmail.com>
2022-02-11 21:41:30 +0000

    Added EmployerAPI class in employers.views.py

--- backend/employers/views.py
+++ backend/employers/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser,FormParser
+from .models import *
+from .serializers import *
 
 # Create your views here.
+
+class EmployerAPI(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+
+    def post(self,request):
+        serializer_class = CreateEmployerSerializer
+        serializer = serializer_class(data = request.data)
+
+        if serializer.is_valid():
+            employer = serializer.save()
+            employer.user = request.user
+            employer.save()
+            return Response(status = status.HTTP_201_CREATED)
+
+        return Response(status = status.HTTP_400_BAD_REQUEST)
