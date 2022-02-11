372a3d2450326cfd7d73cfa4eaeea82c5464ed52
Kelvin <laikelvin8@gmail.com>
2022-02-11 21:40:20 +0000

    Added Employer model class

--- backend/employers/models.py
+++ backend/employers/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
+
+class Employer(models.Model):
+   user = models.OneToOneField(User,on_delete = models.CASCADE, null = True)
+   firstName = models.CharField(max_length = 200)
+   middleName = models.CharField(max_length = 200,blank = True)
+   lastName = models.CharField(max_length = 200)
+   about = models.TextField(blank = True)
+   logo = models.ImageField(upload_to = 'EmployerLogosFiles', blank = True)
+
+   def __str__(self):
+    return self.firstName
+
+
+
+
+    
