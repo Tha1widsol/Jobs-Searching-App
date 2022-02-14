8c9deaae20a1d3d554dff381d0ba90a4d031c9fe
Kelvin <laikelvin8@gmail.com>
2022-02-14 23:35:07 +0000

    Added status property to state

--- frontend/src/components/Global/features/user.ts
+++ frontend/src/components/Global/features/user.ts
@@ -8,15 +8,22 @@ interface UserProps {
         isAnEmployer: boolean | null}
 
     isLoggedIn: boolean | null
-    isLoading: boolean
-    error: any
+    status: null | string
 }
 
 const token = localStorage.getItem('token')
 const email = sessionStorage.getItem('email')
 const isAnEmployer = localStorage.getItem('isAnEmployer')
 
-const initialState: UserProps = {values: {id: null, email: email, isHired: null, isAnEmployer: isAnEmployer === 'true' ? true : isAnEmployer === 'false' ? false : null},isLoggedIn: token ? true : false, isLoading: false, error: null}
+const initialState: UserProps = {values: {
+    id: null,
+    email: email, 
+    isHired: null,
+    isAnEmployer: isAnEmployer === 'true' ? true : isAnEmployer === 'false' ? false : null
+    },
+    isLoggedIn: token ? true : false, 
+    status: null
+}
 
 export const fetchUser:any = createAsyncThunk(
     'user/fetchUser',
@@ -55,17 +62,16 @@ export const userSlice = createSlice({
 
     extraReducers: {
         [fetchUser.pending]: (state) => {
-            state.isLoading = true
-            state.error = null
+            state.status = 'loading'
         },
         
         [fetchUser.fulfilled]: (state,action) => {
             state.values = action.payload
-            state.isLoading = false
+            state.status = 'success'
         },
 
-        [fetchUser.rejected]: (state,action) => {
-            state.error = action.error.message
+        [fetchUser.rejected]: (state) => {
+            state.status = 'rejected'
         }
     }
 
