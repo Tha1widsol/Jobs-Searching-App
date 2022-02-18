8d2f9fffed2df8728b80b94b525c1d5d34983c4c
Kelvin <laikelvin8@gmail.com>
2022-02-18 01:24:29 +0000

    Added CompanySerializer class

--- backend/employers/serializers.py
+++ backend/employers/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
-from .models import Employer
+from .models import *
 
 class EmployerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employer
         fields = '__all__'
-        
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = '__all__'
