9f45c7eb1ff4353642c416b06e11d3a9aad43f2e
Kelvin <laikelvin8@gmail.com>
2022-02-18 01:25:26 +0000

    Added companies paths in employers.urls

--- backend/employers/urls.py
+++ backend/employers/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
-    path('employer',views.EmployerAPI.as_view(),name = 'employer')
+    path('create-company',views.CreateCompanyAPI.as_view(),name = 'company'),
+    re_path(r'^companies/(?P<id>\d+)/$', views.GetCompanyAPI.as_view()),
+    path('companies',views.CompaniesListAPI.as_view(), name = 'companies')
+
 ]
