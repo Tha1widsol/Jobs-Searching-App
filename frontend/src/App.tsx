de0f900cdabf498fb788c12c44925b77aa49d4e4
Kelvin <laikelvin8@gmail.com>
2022-02-19 15:03:42 +0000

    Changed fetchUser import path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React,{useEffect} from 'react'
 import Navbar from './components/Global/Navbar/Navbar'
 import {useAppDispatch} from './components/Global/features/hooks';
 import PagesRoutes from './components/Global/PagesRoutes';
-import {fetchUser} from './components/Global/features/user';
+import {fetchUser} from './components/Global/features/Auth/user';
 import SuccessAlert from './components/Global/messages/SuccessAlert';
 
 function App() {
