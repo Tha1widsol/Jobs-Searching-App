3d6d62190566ff99a89ca614cf8b2cd655d5d5a5
Kelvin <laikelvin8@gmail.com>
2022-02-20 16:06:46 +0000

    Added changes to ProfilePage.tsx

--- frontend/src/components/Jobseekers/ProfilePage/ProfilePage.tsx
+++ frontend/src/components/Jobseekers/ProfilePage/ProfilePage.tsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import './css/ProfilePage.css'
 import Profile from '../Profile/Profile';
-import {useAppSelector} from '../../Global/features/hooks';
-
+import {useParams} from 'react-router-dom';
+import {fetchProfile} from '../../Global/features/Jobseekers/Profile/profile';
+import {useAppSelector,useAppDispatch} from '../../Global/features/hooks';
+import {useNavigate} from 'react-router-dom';
 
 export default function ProfilePage() {
-  const profile = useAppSelector(state => state.profile.values)
+  let navigate = useNavigate()
+  const dispatch = useAppDispatch()
+  const profile = useAppSelector(state => state.profile)
+  const {userID} = useParams()
+
+  useEffect(() => {
+    dispatch(fetchProfile(userID)).then((response: any) => {
+      if (response.error) navigate('/create-profile')
+    })
+ },[dispatch,navigate,userID])
 
   return (
   <div> 
     <label><p id = 'status'>Status:</p></label>
-    <div className = {profile.isActive ? 'public' : 'private'}></div>
-    <Profile userIsOnProfilePage = {true}/>
+    <div className = {profile.values.isActive ? 'public' : 'private'}></div>
+    <Profile profile = {profile} userIsOnProfilePage = {true}/>
   </div>
   
   )
