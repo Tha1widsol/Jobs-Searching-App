1d6c71ae24ebda193123b7cd29ffe32119dc1c45
Kelvin <laikelvin8@gmail.com>
2022-02-23 00:16:19 +0000

    Added CompaniesReducer to redux store in store.ts

--- frontend/src/components/Global/features/store.ts
+++ frontend/src/components/Global/features/store.ts
@@ -1,15 +1,15 @@
 import {configureStore} from '@reduxjs/toolkit'
 import userReducer from './Auth/user'
 import ProfileReducer from './Jobseekers/Profile/profile'
-import EmployerReducer from './Employers/EmployersPage/employer'
 import successMsgReducer from './successMsg'
+import CompaniesReducer from './Employers/Companies/companies'
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     successMsg: successMsgReducer,
     profile: ProfileReducer,
-    employer: EmployerReducer
+    companies: CompaniesReducer
   }
 })
 
