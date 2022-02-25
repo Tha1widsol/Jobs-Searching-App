2da95e960dd84d8409dd53d0a16910e7770aa53c
Kelvin <laikelvin8@gmail.com>
2022-02-25 22:28:05 +0000

    Removed create-company path and added just company instead with CompanyAPI

--- backend/employers/urls.py
+++ backend/employers/urls.py
@@ -2,8 +2,6 @@ from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
-    path('create-company',views.CreateCompanyAPI.as_view(),name = 'company'),
-    re_path(r'^companies/(?P<id>\d+)/$', views.GetCompanyAPI.as_view()),
+    path('company',views.CompanyAPI.as_view(),name = 'company'),
     path('companies',views.CompaniesListAPI.as_view(), name = 'companies')
-
 ]
