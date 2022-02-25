3b11c375ce39e46e43c19ab765b9c38eb722c1f1
Kelvin <laikelvin8@gmail.com>
2022-02-25 22:30:40 +0000

    Added CompanyPage/types.ts

---
+++ frontend/src/components/Employers/CompanyPage/types.ts
@@ -0,0 +1,15 @@
+import {UserProps} from "../../Global/features/Auth/types";
+
+
+export interface CompanyProps{
+    user: UserProps
+    id: number | null
+    name: string
+    email: string
+    about: string
+    phone: string
+    logo?: string
+    banner?: string 
+    industry: string
+    website?: string
+}
