68aaa2f70025a8c64b1381480e63a74d89c32187
Kelvin <laikelvin8@gmail.com>
2022-03-05 16:22:48 +0000

    Added jobs url path

--- backend/employers/urls.py
+++ backend/employers/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path,re_path
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('company',views.CompanyAPI.as_view(),name = 'company'),
-    path('companies',views.CompaniesListAPI.as_view(), name = 'companies')
+    path('companies',views.CompaniesListAPI.as_view(), name = 'companies'),
+    path('jobs',views.JobsListAPI.as_view(), name = 'jobs')
 ]
