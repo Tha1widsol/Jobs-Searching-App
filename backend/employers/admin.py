4bba51c13d3e83ceea968404c4e2543717eb8b8d
Kelvin <laikelvin8@gmail.com>
2022-03-05 16:21:32 +0000

    Registered Job, Role and Benefit class models

--- backend/employers/admin.py
+++ backend/employers/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Employer,Company
+from .models import *
 
 # Register your models here.
 
 admin.site.register(Employer)
 admin.site.register(Company)
+admin.site.register(Job)
+admin.site.register(Role)
+admin.site.register(Benefit)
