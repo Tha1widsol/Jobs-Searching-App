48e3a161d98daeba678c1cee06560f2a5475389e
Kelvin <laikelvin8@gmail.com>
2022-03-09 22:10:02 +0000

    Added ListProps interface class in global forms.ts

--- frontend/src/components/Global/types/forms.ts
+++ frontend/src/components/Global/types/forms.ts
@@ -13,3 +13,15 @@ export interface FileProps{
     value: string | Blob
     name: string
 }
+
+export interface ListProps{
+    value: Array<string>
+    currentVal: string
+    isEmpty: boolean
+    emptyErrorMsg: string
+    alreadyExists: boolean
+    alreadyExistsMsg: string
+    AddedMsg: string
+    RemovedMsg: string
+}
+
