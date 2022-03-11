351cd245c634f074496f2da8128000a145843d2e
Kelvin <laikelvin8@gmail.com>
2022-03-11 23:45:11 +0000

    Made all fields optional

--- backend/employers/models.py
+++ backend/employers/models.py
@@ -42,23 +42,24 @@ class Benefit(models.Model):
             return self.name
 
 class Job(models.Model):
-      user = models.ForeignKey(User,on_delete = models.CASCADE, null = True)
-      company = models.ForeignKey(Company, on_delete = models.CASCADE, null = True)
-      title = models.CharField(max_length = 60, blank = False)
+      user = models.ForeignKey(User,on_delete = models.CASCADE, null = True, blank = True)
+      company = models.ForeignKey(Company, on_delete = models.CASCADE, null = True, blank = True)
+      title = models.CharField(max_length = 60, blank = True)
       description = models.TextField(blank = True)
-      salary = models.CharField(max_length = 50, blank = False)
-      roles = models.ManyToManyField(Role, blank = False)
+      salary = models.CharField(max_length = 50, blank = True)
+      roles = models.ManyToManyField(Role, blank = True)
       industry = models.TextField(default = 'Any')
       remote = models.BooleanField(default = False)
-      training = models.BooleanField(default = False)
+      training = models.BooleanField(default = True)
       positions = models.TextField(default = "1")
       education = models.CharField(max_length = 50)
       skills = models.ManyToManyField(Skill, blank = True)
-      startDate = models.DateField()
+      startDate = models.DateField(null = True,blank = True)
       benefits = models.ManyToManyField(Benefit, blank = True)
       workingDays = models.CharField(max_length = 100, default = 'Monday-Friday')
       workingHours = models.IntegerField(default = 6)
       applicantsCount = models.IntegerField(default = 0)
+      applyOnOwnWebsite = models.BooleanField(default = False)
       datePosted = models.DateTimeField('Job posted',auto_now_add = True)
 
       def __str__(self):
