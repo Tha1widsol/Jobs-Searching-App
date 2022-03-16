5a9a5170891d6edf591080e82595f080020dad72
Kelvin <laikelvin8@gmail.com>
2022-03-16 14:56:59 +0000

    Added post function for job in JopAPI class

--- backend/employers/views.py
+++ backend/employers/views.py
@@ -46,9 +46,62 @@ class CompaniesListAPI(generics.ListAPIView):
         return Response(status = status.HTTP_404_NOT_FOUND)
 
 class JobAPI(APIView):
+    parser_classes = (MultiPartParser, FormParser)
     serializer_class = JobSerializer
 
+    def post(self,request):
+        serializer = self.serializer_class(data = request.data)
+        if serializer.is_valid():
+            job = serializer.save()
+            roles = request.data.get('roles')
+            benefits = request.data.get('benefits')
+            skills = request.data.get('skills')
+            remote = request.data.get('remote')
+            training = request.data.get('training')
+            applyOnOwnWebsite = request.data.get('applyOnOwnWebsite')
 
+            if remote == 'true':
+                job.remote = True
+            
+            if training == 'true':
+                job.training = True
+            
+            if applyOnOwnWebsite == 'true':
+                job.applyOnOwnWebsite = True
+
+            for r in roles.split(','):
+                if not(Role.objects.filter(name = r).exists()):
+                    role = Role(name = r)
+                    role.save()
+
+                else:
+                    role = Role.objects.filter(name = r).first()
+
+                job.roles.add(role)
+
+            for b in benefits.split(','):
+                if not(Benefit.objects.filter(name = b).exists()):
+                    benefit = Benefit(name = b)
+                    benefit.save()
+
+                else:
+                    benefit = Benefit.objects.filter(name = b).first()
+
+                job.benefits.add(benefit)
+
+            for s in skills.split(','):
+                if not(Skill.objects.filter(name = s).exists()):
+                    skill = Skill(name = s)
+                    skill.save()
+                    
+                else:
+                    skill = Skill.objects.filter(name = s).first()
+
+                job.skills.add(skill)
+            
+            job.user = request.user
+            job.save()
+            
 class JobsListAPI(generics.ListAPIView):
     serializer_class = JobSerializer
 
