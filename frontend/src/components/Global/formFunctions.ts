bab0b35865047f37b49eac8b23b3b02b004d4635
Kelvin <laikelvin8@gmail.com>
2022-03-18 00:12:35 +0000

    Added capitalizeFirstCharacter function

--- frontend/src/components/Global/formFunctions.ts
+++ frontend/src/components/Global/formFunctions.ts
@@ -1,17 +1,21 @@
-import React  from "react";
+import React from "react";
 
 export function handleFixName(e: React.KeyboardEvent<HTMLInputElement>){
     const spaceRegex = /\s/g  
-    e.currentTarget.value = e.currentTarget.value.charAt(0).toUpperCase() + e.currentTarget.value.slice(1);  
+    capitalizeFirstCharacter(e)
     e.currentTarget.value = e.currentTarget.value.replace(spaceRegex, '');
     e.currentTarget.value = e.currentTarget.value.charAt(0) + e.currentTarget.value.slice(1).toLowerCase()
 }
 
+export function capitalizeFirstCharacter(e: React.KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>){
+    e.currentTarget.value = e.currentTarget.value.charAt(0).toUpperCase() + e.currentTarget.value.slice(1);  
+}
+
 export const getcurrentDate = () => {
-    const today = new Date();
-    const dd = String(today.getDate() + 1).padStart(2, '0');
-    const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    const yyyy = today.getFullYear();
+    const today = new Date()
+    const dd = String(today.getDate() + 1).padStart(2, '0')
+    const mm = String(today.getMonth() + 1).padStart(2, '0')
+    const yyyy = today.getFullYear()
   
-    return `${yyyy}-${mm}-${dd}`;
+    return `${dd}-${mm}-${yyyy}`
   }
