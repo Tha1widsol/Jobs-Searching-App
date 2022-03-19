f9a78d1053e6861efc4b9c2513f79c8ff9def64e
Kelvin <laikelvin8@gmail.com>
2022-03-19 22:39:34 +0000

    Added employers/jobs.ts

---
+++ frontend/src/components/Global/features/Employers/jobs.ts
@@ -0,0 +1,89 @@
+import {createAsyncThunk,createSlice} from '@reduxjs/toolkit'
+import axios from 'axios'
+
+const token = localStorage.getItem('token')
+
+const initialState = {
+    status: '',
+    values: [{
+        user: {
+            id: 0,
+            email: '',
+            isHired: false,
+            isAnEmployer: false
+        },
+        company: {
+            id: 0,
+            name: '',
+            email: '',
+            about: '',
+            phone: '',
+            logo: '',
+            banner: '',
+            industry: '',
+            website: ''
+        },
+        id: 0,
+        title: '',
+        description: '',
+        salary: '',
+        roles: [{name: ''}],
+        industry: '',
+        remote: false,
+        type: '',
+        training: false,
+        positions: '',
+        education: '',
+        skills: [{name: ''}],
+        startDate: '',
+        benefits: [{name: ''}],
+        workingDays: '',
+        workingHours: '',
+        applicantsCount: 0,
+        applyOnOwnWebsite: false,
+        datePosted: ''
+    }]
+}
+
+export const fetchJobs = createAsyncThunk(
+    'user/fetchJobs',
+    async () => {
+        const response = await axios.get('/api/jobs',{
+            headers: {
+                Authorization: `Token ${token}`
+            }
+        })
+
+        return response.data
+    }
+)
+
+export const JobsSlice = createSlice({
+    name: 'jobs',
+    initialState,
+    reducers:{
+        setJobs: (state,action) => {
+            state.values = action.payload
+        }
+    },
+
+    extraReducers: {
+        [fetchJobs.pending.toString()]: (state) => {
+            state.status = 'loading'
+        },
+
+        [fetchJobs.fulfilled.toString()]: (state,action) => {
+            state.values = action.payload
+            state.status = 'success'
+        },
+
+        [fetchJobs.rejected.toString()]: (state) => {
+            state.status = 'rejected'
+        }
+    }
+
+})
+
+export const {setJobs} = JobsSlice.actions
+
+export default JobsSlice.reducer
