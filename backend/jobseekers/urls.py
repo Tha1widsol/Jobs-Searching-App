761e70c47e8173719aacd9f388dc9928c386cf12
Kelvin <laikelvin8@gmail.com>
2022-03-29 00:39:53 +0100

    Added JobsListAPI path for jobseekers

--- backend/jobseekers/urls.py
+++ backend/jobseekers/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('profile',views.ProfileAPI.as_view(),name = 'profile'),
     path('toggleProfileStatus',views.ToggleProfileStatus.as_view(),name = 'toggleProfileStatus'),
+    path('jobseeker/jobs',views.JobsListAPI.as_view(), name = 'jobs')
 ]
