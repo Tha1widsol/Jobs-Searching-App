8b58b9e84a9917a01b19e46173d8902d90f183cd
Kelvin <laikelvin8@gmail.com>
2022-03-29 18:08:08 +0100

    Added profiles path for ProfilesListAPI

--- backend/employers/urls.py
+++ backend/employers/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('company',views.CompanyAPI.as_view(),name = 'company'),
     path('companies',views.CompaniesListAPI.as_view(), name = 'companies'),
     path('employer/jobs',views.JobsListAPI.as_view(), name = 'jobs'),
-    path('job',views.JobAPI.as_view(), name = 'job')
+    path('job',views.JobAPI.as_view(), name = 'job'),
+    path('profiles',views.ProfilesListAPI.as_view(), name = 'profiles')
 ]
