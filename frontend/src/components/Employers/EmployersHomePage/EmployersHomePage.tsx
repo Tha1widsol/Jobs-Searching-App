95b77caa4048fa116b5eabcf7849bbe12a529f50
Kelvin <laikelvin8@gmail.com>
2022-03-29 18:14:02 +0100

    Dispatched redux profiles state

--- frontend/src/components/Employers/EmployersHomePage/EmployersHomePage.tsx
+++ frontend/src/components/Employers/EmployersHomePage/EmployersHomePage.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import {useAppSelector,useAppDispatch} from '../../Global/features/hooks'
+import {fetchProfiles} from '../../Global/features/Jobseekers/profiles/profiles';
 
 export default function EmployersHomePage() {
+  const dispatch = useAppDispatch()
+  const profiles = useAppSelector(state => state.profiles)
+  const [dropdown,setDropdown] = useState<number | null>(null)
+
+  useEffect(() => {
+    dispatch(fetchProfiles())
+  },[dispatch])
+
   return (
         <div>
             <h1>Employers home page</h1>
+            {profiles.values?.map(profile => {
+                return(
+                    <h2>{profile.firstName}</h2>
+                )
+            })}
         </div>
     )
 }
