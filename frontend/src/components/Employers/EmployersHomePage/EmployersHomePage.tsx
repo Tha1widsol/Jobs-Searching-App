ca0de479369e5764e3c9fa3e9f595d3fe3467cfa
Kelvin <laikelvin8@gmail.com>
2022-03-31 15:36:26 +0100

    Added content in EmployersHomePage.tsx

--- frontend/src/components/Employers/EmployersHomePage/EmployersHomePage.tsx
+++ frontend/src/components/Employers/EmployersHomePage/EmployersHomePage.tsx
@@ -13,12 +13,48 @@ export default function EmployersHomePage() {
 
   return (
         <div>
-            <h1>Employers home page</h1>
-            {profiles.values?.map(profile => {
-                return(
-                    <h2>{profile.firstName}</h2>
-                )
-            })}
+            <label><h2>Potential candidates...</h2></label>
+            <section style = {{display: 'flex', marginRight: '15px'}}>
+                {profiles.values?.map((profile,index) => {
+                    return(
+                        <div className = 'featuredContainer' key = {index}>
+                             <section onMouseEnter = {() => setDropdown(index)} onMouseLeave = {() => setDropdown(null)}>
+                                <div className = 'kebabMenuIcon'/>
+                                <div className = 'containerDropdown'>
+                                {dropdown === index ? 
+                                    <div className = 'containerDropdownContent'>
+                                        <a href = {`profile/${profile.user.id}`}><button className = 'dropdownBtn'>View</button></a> 
+                                        <button className = 'dropdownBtn'>Hide</button>
+                                        <button className = 'deleteNavBtn'>Report</button>
+                                    </div>
+                                : null}
+                                </div>
+                              </section>
+
+                            <a href = {`/profile/${profile.user.id}`}><h2>{profile.firstName} {profile.middleName} {profile.lastName}</h2></a>
+                            <p>(Looking for work)</p>
+                            <p><i className = 'fa fa-phone icon'></i>000011111</p>
+                            <p><i className = 'icon'>&#9993;</i>{profile.user.email}</p>
+                            <hr className = 'mt-0-mb-4'/>
+
+                            <label><h3>Skills:</h3></label>
+                            <section className = 'listContainer'>
+                                {profile.skills.map((skill,index) => {
+                                    return (<li key = {index}>{skill.name}</li>)
+                                })}
+                            </section>
+
+                            <hr className = 'mt-0-mb-4'/>
+                            <p className = 'containerText'>{profile.experience}</p>
+                            <section style = {{textAlign:'center'}}>
+                                <button>Shortlist</button>
+                                <button>Message</button>
+                            </section>   
+                        </div>
+                    )
+                })}
+            </section>
+
         </div>
     )
 }
