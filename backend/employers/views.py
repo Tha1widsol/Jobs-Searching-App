ec28f7ab568d60ed9f62fd2d0e3eddbc34ab62d7
Kelvin <laikelvin8@gmail.com>
2022-04-12 21:53:24 +0100

    Added put function for JobAPI class

--- backend/employers/views.py
+++ backend/employers/views.py
@@ -122,6 +122,71 @@ class JobAPI(APIView):
 
         return Response(status = status.HTTP_400_BAD_REQUEST)
 
+    
+    def put(self,request):
+        lookup_url_kwarg ='id'
+        id = request.GET.get(lookup_url_kwarg)
+        job = Job.objects.get(id = id)
+        serializer = self.serializer_class(data = request.data, instance = job)
+        job.skills.clear()
+        job.roles.clear()
+        job.benefits.clear()
+
+        if serializer.is_valid():
+            job = serializer.save()
+            roles = request.data.get('roles')
+            benefits = request.data.get('benefits')
+            skills = request.data.get('skills')
+            remote = request.data.get('remote')
+            training = request.data.get('training')
+            applyOnOwnWebsite = request.data.get('applyOnOwnWebsite')
+
+            if remote == 'true':
+                job.remote = True
+            
+            if training == 'true':
+                job.training = True
+            
+            if applyOnOwnWebsite == 'true':
+                job.applyOnOwnWebsite = True
+
+            for r in roles.split(','):
+                if not(Role.objects.filter(name = r).exists()):
+                    role = Role(name = r)
+                    role.save()
+
+                else:
+                    role = Role.objects.filter(name = r).first()
+
+                job.roles.add(role)
+
+            for b in benefits.split(','):
+                if not(Benefit.objects.filter(name = b).exists()):
+                    benefit = Benefit(name = b)
+                    benefit.save()
+
+                else:
+                    benefit = Benefit.objects.filter(name = b).first()
+
+                job.benefits.add(benefit)
+
+            for s in skills.split(','):
+                if not(Skill.objects.filter(name = s).exists()):
+                    skill = Skill(name = s)
+                    skill.save()
+                    
+                else:
+                    skill = Skill.objects.filter(name = s).first()
+
+                job.skills.add(skill)
+            
+            job.user = request.user
+            job.save()
+            return Response(status = status.HTTP_200_OK)
+
+        return Response(status = status.HTTP_400_BAD_REQUEST)
+        
+
     def get(self,request):
         lookup_url_kwarg ='id'
         id = request.GET.get(lookup_url_kwarg)
