c7d0c5c8a691c273feada69002bfaf4f485cb2aa
Kelvin <laikelvin8@gmail.com>
2022-04-21 21:34:53 +0100

    Added KebabMenu component tag

--- frontend/src/components/Jobseekers/JobSeekersHomePage/JobSeekersHomePage.tsx
+++ frontend/src/components/Jobseekers/JobSeekersHomePage/JobSeekersHomePage.tsx
@@ -1,6 +1,7 @@
 import React,{useState,useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import {useAppSelector,useAppDispatch} from '../../Global/features/hooks'
+import KebabMenu from '../../Global/KebabMenu/KebabMenu';
 import {fetchJobs} from '../../Global/features/Employers/jobs/jobs'
 
 export default function JobSeekersHomePage() {
@@ -19,19 +20,12 @@ export default function JobSeekersHomePage() {
         {jobs.values?.map((job,index) => {
           return(
             <div className = 'featuredContainer' key = {index}>
-               <section onMouseEnter = {() => setDropdown(index)} onMouseLeave = {() => setDropdown(null)}>
-                <div className = 'kebabMenuIcon'/>
-                <div className = 'containerDropdown'>
-                  {dropdown === index ? 
-                    <div className = 'containerDropdownContent'>
-                        <a href = {`job/${job.id}`}><button className = 'dropdownBtn'>View</button></a> 
-                        <button className = 'dropdownBtn'>Hide</button>
-                        <button className = 'deleteNavBtn'>Report</button>
-                    </div>
-                : null}
-                </div>
-              </section>
-
+              <KebabMenu current = {dropdown} many = {true} index = {index} switchOn = {() => setDropdown(index)} switchOff = {() => setDropdown(null)}>
+                <Link to = {`/job/${job.id}`}><button className = 'dropdownBtn'>View</button></Link> 
+                <button className = 'dropdownBtn'>Hide</button>
+                <button className = 'deleteNavBtn'>Report</button>
+              </KebabMenu>
+                   
               <Link to = {`/job/${job.id}`}><h2>{job.title}</h2></Link>
               <Link to = {`/company/${job.company?.id}`}><p>{job.company?.name}</p></Link>
               {job.salary2 ? <p>{job.currency}{job.salary1} - {job.currency}{job.salary2} a year </p> : <p>{job.currency}{job.salary1} a year</p>} 
