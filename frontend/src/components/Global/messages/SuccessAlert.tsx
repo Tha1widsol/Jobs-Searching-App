f1e6a08e5f20d290e52223c0d417786ff5aff7f5
Kelvin <laikelvin8@gmail.com>
2022-04-21 11:54:42 +0100

    Added handleAddSuccessMsg function

--- frontend/src/components/Global/messages/SuccessAlert.tsx
+++ frontend/src/components/Global/messages/SuccessAlert.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import {Dispatch} from 'redux';
 import {useAppSelector} from '../features/hooks';
+import {setMessage} from '../../Global/features/successMsg';
+
+export function handleAddSuccessMsg(msg: string, dispatch: Dispatch, timer = 2000){
+    dispatch(setMessage(msg))
+    window.scrollTo(0, 0)
+    setTimeout(() => {
+    dispatch(setMessage(''))
+    }, timer)
+}
 
 export default function SuccessAlert() {
   const successMsg = useAppSelector(state => state.successMsg.value)
