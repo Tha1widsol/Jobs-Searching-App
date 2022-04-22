544c3aa4ac48f500b4e0a013cca41d0391790081
Kelvin <laikelvin8@gmail.com>
2022-04-22 15:55:06 +0100

    Added popup in EmployerJobsPage.tsx

--- frontend/src/components/Employers/EmployerJobsPage/EmployerJobsPage.tsx
+++ frontend/src/components/Employers/EmployerJobsPage/EmployerJobsPage.tsx
@@ -1,20 +1,55 @@
 import React,{useState,useEffect} from 'react'
-import {Link} from 'react-router-dom'
+import {useNavigate,Link} from 'react-router-dom'
 import {useAppSelector,useAppDispatch} from '../../Global/features/hooks'
 import KebabMenu from '../../Global/KebabMenu/KebabMenu'
+import {token} from '../../Global/features/Auth/user'
+import Popup from '../../Global/Popup/Popup'
 import {fetchJobs} from '../../Global/features/Employers/jobs/jobs'
+import {setJobs} from '../../Global/features/Employers/jobs/jobs'
+import {handleAddSuccessMsg} from '../../Global/messages/SuccessAlert'
+import axios from 'axios'
 
 export default function EmployerJobsPage() {
   const dispatch = useAppDispatch()
+  const navigate = useNavigate()
   const jobs = useAppSelector(state => state.jobs)
+  const [chosenJob,setChosenJob] = useState({id: 0, title: ''})
+  const [popup,setPopup] = useState(false)
   const [dropdown,setDropdown] = useState<number | null>(null)
 
   useEffect(() => {
     dispatch(fetchJobs('employer'))
   },[dispatch])
 
+  function ShowPopup(id: number, title: string){
+    setChosenJob({id: id, title: title})
+    setPopup(true)
+  }
+
+  function handleDeleteJob(){
+    axios.delete(`/api/job?id=${chosenJob.id}`,{headers: {Authorization: `Token ${token}`}})
+    .then(response => {
+    if (response.status === 200){
+        const newJobs = [...jobs.values]
+        let index = newJobs.findIndex(job => job.id === chosenJob.id)
+        newJobs.splice(index, 1)
+        dispatch(setJobs(newJobs))
+        handleAddSuccessMsg('Job is successfully removed', dispatch)
+        navigate('/jobs')
+        setPopup(false)
+    } 
+
+  })
+}
+
   return (
     <div>
+      <Popup trigger = {popup} switchOff = {() => setPopup(false)}>
+          <p>Are you sure you want to remove {chosenJob.title}?</p>
+          <p style = {{fontSize: 'small'}}>(This action cannot be undone)</p>
+          <button onClick = {handleDeleteJob}>Confirm</button>
+      </Popup>
+
       <h1 className = 'title'>Jobs</h1>
 
        {jobs.values?.map((job,index) => {
@@ -22,7 +57,7 @@ export default function EmployerJobsPage() {
              <div className = 'Container' key = {index}>
               <KebabMenu current = {dropdown} many = {true} index = {index} switchOn = {() => setDropdown(index)} switchOff = {() => setDropdown(null)}>
                 <Link to = {`/edit-job/${job.id}`}><button className = 'dropdownBtn'>Edit</button></Link>
-                <button className = 'deleteNavBtn'>Delete</button>
+                <button className = 'deleteNavBtn' onClick = {() => ShowPopup(job.id,job.title)}>Delete</button>
               </KebabMenu>
 
                 <section style = {{display: 'flex'}}>
