8a5c832df33b236be8a6098777608f2218eb9287
Kelvin <laikelvin8@gmail.com>
2022-05-01 16:14:09 +0100

    Added checkApplication path

--- backend/jobseekers/urls.py
+++ backend/jobseekers/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('profile',views.ProfileAPI.as_view(),name = 'profile'),
     path('toggleProfileStatus',views.ToggleProfileStatus.as_view(),name = 'toggleProfileStatus'),
     path('jobseeker/jobs',views.JobsListAPI.as_view(), name = 'jobs'),
-    path('application',views.ApplicationAPI.as_view(), name = 'application')
+    path('application',views.ApplicationAPI.as_view(), name = 'application'),
+    path('checkApplication',views.checkApplicationExists, name = 'checkApplication')
 ]
