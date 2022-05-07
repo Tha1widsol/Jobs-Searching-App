837fba370a0f9ec6e901e431b4b6e650c3a9de84
Kelvin <laikelvin8@gmail.com>
2022-05-07 16:43:58 +0100

    Added KebabMenu tag and application content to containers

--- frontend/src/components/Jobseekers/JobSeekerApplicationsPage.tsx/JobSeekerApplicationsPage.tsx
+++ frontend/src/components/Jobseekers/JobSeekerApplicationsPage.tsx/JobSeekerApplicationsPage.tsx
@@ -1,8 +1,9 @@
-import React,{useEffect} from 'react'
+import React,{useState,useEffect} from 'react'
 import {Link,useLocation} from 'react-router-dom'
 import {useAppSelector,useAppDispatch} from '../../Global/features/hooks'
 import {fetchApplications} from '../../Global/features/Jobseekers/applications/applications'
 import './css/JobSeekerApplicationsPage.css'
+import KebabMenu from '../../Global/KebabMenu/KebabMenu';
 
 function useQuery(){
     return new URLSearchParams(useLocation().search)
@@ -10,6 +11,7 @@ function useQuery(){
 
 export default function JobSeekerApplicationsPage() {
   let query = useQuery()
+  const [dropdown,setDropdown] = useState<number | null>(null)
   const tab = query.get('tab')
   const dispatch = useAppDispatch();
   const applications = useAppSelector(state => state.applications)
@@ -30,8 +32,26 @@ export default function JobSeekerApplicationsPage() {
                 applications.values?.map((application, index) => {
                     return (
                         <div className = 'Container' key = {index}>
-                            <p>{application.job.title}</p>
-                            <p>{application.job.company.name}</p>
+                            <KebabMenu current = {dropdown} many = {true} index = {index} switchOn = {() => setDropdown(index)} switchOff = {() => setDropdown(null)}>
+                                <Link to = {`/job/${application.job.id}`}><button className = 'dropdownBtn'>View</button></Link> 
+                                <button className = 'dropdownBtn'>Hide</button>
+                                <button className = 'deleteNavBtn'>Delete</button>
+                           </KebabMenu>
+
+                           <Link to = {`/job/${application.job.id}`}><h2>{application.job.title}</h2></Link>
+                           <Link to = {`/company/${application.job.company?.id}`}><p>{application.job.company.name}</p></Link>
+
+                            <hr className = 'mt-0-mb-4'/>
+                            <section style = {{display: 'flex', alignItems: 'center',}}>
+                                <p style = {{marginRight: '5px'}}>Status:</p>
+                                <div className = {application.status === 'applied' ? 'indicator amber' : applications.status === 'accepted' ? 'indicator green' : application.status === 'rejected' ? 'indicator red' : ''}></div>
+                            </section>
+        
+                            <section style = {{fontSize: 'small'}}>
+                                <p>Start date: {application.job.startDate}</p>
+                                <p>Applied date: {application.applicationDate.substring(0,10)}</p>
+                            </section>
+                            <button>Track</button>
                         </div>
                     )
                 })
