804a5d2841cd720ac1465cef5dffa833f15a9287
Kelvin <laikelvin8@gmail.com>
2022-05-11 17:28:33 +0100

    Referenced User model without import

--- backend/jobseekers/models.py
+++ backend/jobseekers/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from accounts.models import User
 from main.models import Skill
 
 # Create your models here.
 
 class Profile(models.Model):
-   user = models.OneToOneField(User,on_delete = models.CASCADE,null = True)
+   user = models.OneToOneField('accounts.user',on_delete = models.CASCADE,null = True)
    firstName = models.CharField(max_length = 200)
    middleName = models.CharField(max_length = 200,blank = True)
    lastName = models.CharField(max_length = 200)
