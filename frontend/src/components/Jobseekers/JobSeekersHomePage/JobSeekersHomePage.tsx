e7e31d7d7c77563faf80294b3ad746719774be8b
Kelvin <laikelvin8@gmail.com>
2022-05-19 22:58:45 +0100

    Called dispatch fetchSavedJobs again after saving

--- frontend/src/components/Jobseekers/JobSeekersHomePage/JobSeekersHomePage.tsx
+++ frontend/src/components/Jobseekers/JobSeekersHomePage/JobSeekersHomePage.tsx
@@ -27,6 +27,7 @@ export default function JobSeekersHomePage() {
       
     })
     .then(() => {
+      dispatch(fetchSavedJobs())
       handleAddSuccessMsg('Job is successfully saved', dispatch)
     })
   }
