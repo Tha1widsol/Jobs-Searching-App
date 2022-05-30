5f1de2ebf1744129a48f2fc3d308caf6b746e693
Kelvin <laikelvin8@gmail.com>
2022-05-30 23:13:33 +0100

    Added MatchingJobsSerializer class

--- backend/employers/serializers.py
+++ backend/employers/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
-from jobseekers.serializers import ProfileSerializer
+from jobseekers.serializers import ProfileSerializer, SkillSerializer
 from main.serializers import UserSerializer
-from jobseekers.serializers import SkillSerializer
+from main.models import Match
 from .models import *
+
 
 class EmployerSerializer(serializers.ModelSerializer):
     class Meta:
@@ -42,4 +43,13 @@ class ApplicationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Application
+        fields = '__all__'
+
+class MatchingJobsSerializer(serializers.ModelSerializer):
+    employer = UserSerializer(read_only = True)
+    company = CompanySerializer(read_only = True)
+    job = JobSerializer(read_only = True)
+
+    class Meta:
+        model = Match
         fields = '__all__'
