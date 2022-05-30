83a84006832cf983e25a2fa374e8b1c0eb60f033
Kelvin <laikelvin8@gmail.com>
2022-05-30 23:19:06 +0100

    Dispatched fetchMatchingJobs reducer

--- frontend/src/components/Jobseekers/JobSeekersHomePage/JobSeekersHomePage.tsx
+++ frontend/src/components/Jobseekers/JobSeekersHomePage/JobSeekersHomePage.tsx
@@ -2,7 +2,7 @@ import React,{useState,useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import {useAppSelector,useAppDispatch} from '../../Global/features/hooks'
 import KebabMenu from '../../Global/KebabMenu/KebabMenu';
-import {fetchJobs} from '../../Global/features/Employers/jobs/jobs'
+import {fetchMatchingJobs} from '../../Global/features/Jobseekers/matchingJobs/matchingJobs';
 import {fetchSavedJobs} from '../../Global/features/Jobseekers/savedJobs/savedJobs';
 import {handleAddSuccessMsg} from '../../Global/messages/SuccessAlert';
 import {token} from '../../Global/features/Auth/user';
@@ -10,22 +10,16 @@ import axios from 'axios'
 
 export default function JobSeekersHomePage() {
   const dispatch = useAppDispatch()
-  const jobs = useAppSelector(state => state.jobs)
+  const matchingJobs = useAppSelector(state => state.matchingJobs)
   const savedJobs = useAppSelector(state => state.savedJobs)
   const [dropdown,setDropdown] = useState<number | null>(null)
-  const [matches,setMatches] = useState<Array<number>>([])
+
 
   useEffect(() => {
-    axios.get('/api/getMatchingScores',{
-      headers: {
-          Authorization:`Token ${token}`
-      }
-  })
-  
-    dispatch(fetchJobs('jobseeker'))
+    dispatch(fetchMatchingJobs())
     dispatch(fetchSavedJobs())
- 
   },[dispatch])
+  
   function handleSaveJob(id: number){
     axios.post(`/api/save-job?id=${id}`,null,{
       headers: {
@@ -44,36 +38,36 @@ export default function JobSeekersHomePage() {
       <label><h2>Potential job matches based on your profile...</h2></label>
       <section style = {{display: 'flex', marginRight: '15px'}}>
         
-        {jobs.values?.map((job,index) => {
+        {matchingJobs.values?.map((matching,index) => {
           return(
             <div className = 'featuredContainer' key = {index}>
               <KebabMenu current = {dropdown} many = {true} index = {index} switchOn = {() => setDropdown(index)} switchOff = {() => setDropdown(null)}>
-                <Link to = {`/job/${job.id}`}><button className = 'dropdownBtn'>View</button></Link> 
+                <Link to = {`/job/${matching.job?.id}`}><button className = 'dropdownBtn'>View</button></Link> 
                 <button className = 'dropdownBtn'>Hide</button>
                 <button className = 'dropdownBtn redNavBtn'>Report</button>
               </KebabMenu>
 
-                <Link to = {`/job/${job.id}`}><h2>{job.title}</h2></Link>
-                <p style = {{color: 'gray'}}> {matches[job.id]}% - matching score</p>
+                <Link to = {`/job/${matching.job?.id}`}><h2>{matching.job?.title}</h2></Link>
+                <p style = {{color: 'gray'}}> {matching.score}% - matching score</p>
             
-              <Link to = {`/company/${job.company?.id}`}><p>{job.company?.name}</p></Link>
-              {job.salary2 ? <p>{job.currency}{job.salary1} - {job.currency}{job.salary2} a year </p> : <p>{job.currency}{job.salary1} a year</p>} 
-              <p>{job.type}</p>
+              <Link to = {`/company/${matching.company?.id}`}><p>{matching.company?.name}</p></Link>
+              {matching.job?.salary2 ? <p>{matching.job?.currency}{matching.job?.salary1} - {matching.job?.currency}{matching.job?.salary2} a year </p> : <p>{matching.job?.currency}{matching.job?.salary1} a year</p>} 
+              <p>{matching.job?.type}</p>
               <hr className = 'mt-0-mb-4'/>
-              <p className = 'containerText'>{job.description}</p>
+              <p className = 'containerText'>{matching.job?.description}</p>
           
               <label><h3>Roles:</h3></label>
                 <section className = 'listContainer' style = {{marginBottom: '20px'}}>
-                    {job.roles.map((role,index) => {
+                    {matching.job?.roles.map((role,index) => {
                         return (<li key = {index}>{role.name}</li>)
                     })}
                 </section>
              
               <section style = {{textAlign:'center'}}>
-                  {job.applyOnOwnWebsite ? 
-                   <a href = {job.link} target = 'blank'><button>Apply Externally</button></a>
-                  : <Link to = {`/apply/${job.id}`}><button>Apply</button></Link>}
-                  {!savedJobs.values?.find(savedJob => savedJob.job.id === job.id) ? <button onClick = {() => handleSaveJob(job.id)}>Save</button> : null}
+                  {matching.job?.applyOnOwnWebsite ? 
+                   <a href = {matching.job?.link} target = 'blank'><button>Apply Externally</button></a>
+                  : <Link to = {`/apply/${matching.job?.id}`}><button>Apply</button></Link>}
+                  {!savedJobs.values?.find(savedJob => savedJob.job.id === matching.job?.id) ? <button onClick = {() => handleSaveJob(matching.job?.id)}>Save</button> : null}
               </section>   
 
             </div>
