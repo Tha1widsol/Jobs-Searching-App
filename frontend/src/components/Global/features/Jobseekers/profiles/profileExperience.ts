9112dae7109dc34ff195d6f2e0300ffe1690368a
Kelvin <laikelvin8@gmail.com>
2022-06-22 20:51:08 +0100

    Added profileExperience.ts redux file

---
+++ frontend/src/components/Global/features/Jobseekers/profiles/profileExperience.ts
@@ -0,0 +1,65 @@
+import {createAsyncThunk,createSlice} from '@reduxjs/toolkit'
+import {token} from '../../Auth/user'
+import axios from 'axios'
+
+const initialState = {
+    status: '',
+    values: {
+        id: 0,
+        title: '',
+        companyName: '',
+        description: '',
+        years: 0,
+        reference: '',
+        location: '',
+        ongoing: false
+    }
+
+}
+  
+export const fetchProfileExperience = createAsyncThunk(
+    'user/fetchProfileExperience',
+    async (id: number) => {
+        const response = await axios.get(`/api/profileExperience?id=${id}`,{
+            headers: {
+                Authorization:`Token ${token}`
+            }
+        })
+
+    return response.data
+    }
+)
+
+export const ProfileExperienceSlice = createSlice({
+    name : 'profileExperience',
+    initialState,
+    reducers: {
+        setProfile: (state,action) => {
+          state.values = action.payload
+        },
+
+        setDeleteProfileExperience: (state) => {
+            state.values = initialState.values
+        }
+    },
+
+    extraReducers: {
+        [fetchProfileExperience.pending.toString()]: (state) => {
+            state.status = 'loading'
+        },
+        
+        [fetchProfileExperience.fulfilled.toString()]: (state,action) => {
+            state.values = action.payload
+            state.status = 'success'
+        },
+
+        [fetchProfileExperience.rejected.toString()]: (state) => {
+            state.status = 'rejected'
+    }
+  }
+
+})
+
+export const {setProfile,setDeleteProfileExperience} = ProfileExperienceSlice.actions
+
+export default ProfileExperienceSlice.reducer
