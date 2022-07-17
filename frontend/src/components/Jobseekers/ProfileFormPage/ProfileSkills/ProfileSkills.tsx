4833eb18e2e76b1c2b7f2f8fb738c6bfa2656bd5
Kelvin <laikelvin8@gmail.com>
2022-07-17 18:04:47 +0100

    Added ProfileSkills.tsx

---
+++ frontend/src/components/Jobseekers/ProfileFormPage/ProfileSkills/ProfileSkills.tsx
@@ -0,0 +1,38 @@
+import React,{useState, useEffect} from 'react'
+import {useAppSelector} from '../../../Global/features/hooks'
+import {ListProps} from '../../../Global/types/forms'
+import List from '../../../Global/Forms/List'
+
+export default function ProfileSkills({edit = false}) {
+    const [skills, setSkills] = useState<ListProps>({value: [], currentVal: '',isEmpty: false, emptyErrorMsg: 'Invalid skill', alreadyExists: false, alreadyExistsMsg: 'Skill already exists',AddedMsg:'Skill added',RemovedMsg: 'Skill removed'})
+    const profile = useAppSelector(state => state.profile.values)
+
+    useEffect(() => {
+        if (!edit) return
+        setSkills(prev => {return{...prev, value: profile.skills.map(skill => skill.name)}})
+    },[profile.skills, edit])
+
+    function handleSetSkills(e: React.ChangeEvent<HTMLInputElement>){
+        setSkills(prev => {return {...prev, currentVal: e.target.value}})
+        e.target.value = e.target.value.replace(',','')
+    }
+
+  return (
+    <div>
+        <h1 className = 'title'>Skills</h1> 
+        <label htmlFor = 'skills'><h3>Specific Key skills:</h3></label>
+        <input id = 'skills' className = {skills.alreadyExists || skills.isEmpty ? 'inputError' : ''} value = {skills.currentVal} onChange = {handleSetSkills} placeholder = 'E.g Good problem solving...' autoComplete = 'on' required/>
+        
+        <List name = 'Skills' 
+        state = {skills}
+        handleAdd = {() => setSkills(prev => ({...prev, value: [...prev.value, skills.currentVal]}))}
+        handleClearInput = {() => setSkills(prev => {return {...prev,currentVal: ''}})}
+        handleSetIsEmpty = {(empty = true) => setSkills(prev => {return{...prev,isEmpty: empty}})}
+        handleSetAlreadyExists = {(exists = true) => setSkills(prev => {return {...prev,alreadyExists: exists}})}
+        handleSetAll = {(newItems: Array<string>) => setSkills(prev => {return {...prev,value: newItems}})}
+        />
+
+        {skills.value.length ? <button type = 'submit'>Submit</button> : null}
+    </div>
+  )
+}
