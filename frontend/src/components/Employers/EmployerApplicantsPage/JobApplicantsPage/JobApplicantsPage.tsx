7ba82ffbb2a27ca9374407211e4bb9f04642d3b0
Kelvin <laikelvin8@gmail.com>
2022-08-16 14:21:13 +0100

    Added condition for applicants

--- frontend/src/components/Employers/EmployerApplicantsPage/JobApplicantsPage/JobApplicantsPage.tsx
+++ frontend/src/components/Employers/EmployerApplicantsPage/JobApplicantsPage/JobApplicantsPage.tsx
@@ -17,37 +17,48 @@ export default function JobApplicantsPage() {
 
   return (
     <div>
-        <div style = {{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-          <h1>Applications for <Link to = {`/job/${jobID}`}>{jobTitle}:</Link></h1>
-          <button>Screen applicants</button>
+        {applicants.values?.length ? 
+        <div>
+           <div style = {{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+              <h1>Applications for <Link to = {`/job/${jobID}`}>{jobTitle}:</Link></h1>
+              <button>Screen applicants</button>
+           </div>
+
+            <hr className = 'mt-0-mb-4' style = {{marginBottom: '20px'}}/>
+            {applicants.values?.map((applicant, index) => {
+              return (
+                <div className = 'Container' key = {index}>
+                <KebabMenu current = {dropdown} many = {true} index = {index} switchOn = {() => setDropdown(index)} switchOff = {() => setDropdown(null)}>
+                    <Link to = {`/profile/${applicant.profile.id}`}><button className = 'dropdownBtn'>View</button></Link>
+                    <button className = 'dropdownBtn'>Track</button>
+                    <button className = 'dropdownBtn redNavBtn'>Reject</button>
+                </KebabMenu>
+                
+                <Link to = {`/profile/${applicant.profile.id}`}><h2>{applicant.profile.firstName} {applicant.profile.lastName}</h2></Link>
+                <section style = {{display: 'flex', alignItems: 'center',}}>
+                        <p style = {{marginRight: '5px'}}>Status:</p>
+                        <div className = {applicant.status === 'applied' ? 'indicator amber' : applicant.status === 'accepted' ? 'indicator green' : applicant.status === 'rejected' ? 'indicator red' : ''}></div>
+                </section>
+
+                <section style = {{fontSize: 'small', color: 'gray'}}>
+                        <p>Start date of job: {applicant.job.startDate}</p>
+                        <p>Applied date: {applicant.applicationDate.substring(0,10)}</p>
+                </section>
+
+                <button>Move forward</button>
+                <button>Hire</button>
+            </div>
+              )
+            })}
         </div>
-
-        <hr className = 'mt-0-mb-4' style = {{marginBottom: '20px'}}/>
-        {applicants.values?.map((applicant, index) => {
-          return (
-            <div className = 'Container' key = {index}>
-            <KebabMenu current = {dropdown} many = {true} index = {index} switchOn = {() => setDropdown(index)} switchOff = {() => setDropdown(null)}>
-                <Link to = {`/profile/${applicant.profile.id}`}><button className = 'dropdownBtn'>View</button></Link>
-                <button className = 'dropdownBtn'>Track</button>
-                <button className = 'dropdownBtn redNavBtn'>Reject</button>
-            </KebabMenu>
-            
-            <Link to = {`/profile/${applicant.profile.id}`}><h2>{applicant.profile.firstName} {applicant.profile.lastName}</h2></Link>
-            <section style = {{display: 'flex', alignItems: 'center',}}>
-                    <p style = {{marginRight: '5px'}}>Status:</p>
-                    <div className = {applicant.status === 'applied' ? 'indicator amber' : applicant.status === 'accepted' ? 'indicator green' : applicant.status === 'rejected' ? 'indicator red' : ''}></div>
-            </section>
-
-            <section style = {{fontSize: 'small', color: 'gray'}}>
-                    <p>Start date of job: {applicant.job.startDate}</p>
-                    <p>Applied date: {applicant.applicationDate.substring(0,10)}</p>
-            </section>
-
-            <button>Move forward</button>
-            <button>Hire</button>
-        </div>
-          )
-        })}
+        
+        : 
+        <>
+        <h2>No applicants yet...</h2>
+         <Link to = '/'><button>Find profiles</button></Link> 
+        </>
+        }
+       
     </div>
   )
 }
