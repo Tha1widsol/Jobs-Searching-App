0028e0d6506e15ca78407bd3cef6c734d72d52b6
Kelvin <laikelvin8@gmail.com>
2022-09-05 09:51:27 +0100

    Added PublicHomePage.tsx

--- frontend/src/components/public/PublicHomePage/PublicHomePage.tsx
+++ frontend/src/components/public/PublicHomePage/PublicHomePage.tsx
@@ -1,28 +1,36 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import {Link} from 'react-router-dom'
+import { useAppSelector, useAppDispatch } from '../../Global/features/hooks'
+import JobSearchBar from '../SearchBar/JobSearchBar'
+import {useQuery} from '../../Global/features/hooks';
 import './css/HomePage.css'
 
 export default function PublicHomePage() {
+    const dispatch = useAppDispatch()
+    
     return (
-        <div style = {{textAlign:'center'}}>
-            <h1 id = 'youAre'>You are...</h1>
+        <div>
+            <JobSearchBar/>
+            <div style = {{textAlign:'center'}}>
+                <h1 id = 'youAre'>You are...</h1>
 
-            <section id = 'containers'>
-                <section className = 'container'>
-                    <h2 className = 'heading'><u>Looking for a job:</u></h2>
-                    <p>Find your dream job right now with one of the most trusted job searching engines on the web.</p>
-                    <Link to = '/register?choice=jobseeker' ><button id = 'jobseeker' >Add your profile</button></Link> 
-                </section>
-                
-                <div id = 'divider'/>
+                <section id = 'containers'>
+                    <section className = 'container'>
+                        <h2 className = 'heading'><u>Looking for a job:</u></h2>
+                        <p>Find your dream job right now with one of the most trusted job searching engines on the web.</p>
+                        <Link to = '/register?choice=jobseeker' ><button id = 'jobseeker' >Add your profile</button></Link> 
+                    </section>
+                    
+                    <div id = 'divider'/>
 
-                <section className = 'container'>
-                    <h2 className = 'heading'><u>An employer:</u></h2>
-                    <p>Setup your company and start adding jobs using our system.</p>
-                    <Link to = '/register?choice=employer' ><button id = 'company' >Add your company</button></Link> 
+                    <section className = 'container'>
+                        <h2 className = 'heading'><u>An employer:</u></h2>
+                        <p>Setup your company and start adding jobs using our system.</p>
+                        <Link to = '/register?choice=employer' ><button id = 'company' >Add your company</button></Link> 
+                    </section>
                 </section>
-            </section>
-          
+            
+            </div>
         </div>
     )
 }
