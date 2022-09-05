f4bfc75c744cbe907f1304d8930bf525083a6f80
Kelvin <laikelvin8@gmail.com>
2022-09-05 09:54:31 +0100

    Added HomePageJobsAPI class

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('user',views.getCurrentUser.as_view(),name = 'currentUser')
+    path('user',views.getCurrentUser.as_view(),name = 'currentUser'),
+    path('/homePageJobs', views.HomePageJobsAPI.as_view(), name = 'homeJobs')
 ]
--- backend/main/views.py
+++ backend/main/views.py
@@ -1,6 +1,10 @@
 from rest_framework.views import APIView
+from rest_framework import generics
+from employers.serializers import JobSerializer
 from rest_framework.response import Response
 from .models import *
+from employers.models import Job
+from django.db.models import Q
 from .serializers import *
 
 # Create your views here.
@@ -9,3 +13,15 @@ class getCurrentUser(APIView):
     def get(self,request,*args,**kwargs):
         serializer_class = UserSerializer(request.user)
         return Response(serializer_class.data)
+
+
+class HomePageJobsAPI(generics.ListAPIView):
+     serializer_class = JobSerializer
+
+     def get_queryet(self):
+        query = self.request.GET.get('q' ,'')
+        jobs = Match.objects.filter(
+        Q(job__title__icontains = query)|
+        Q(job__description__icontains = query)|
+        Q(job__company__name__icontains = query))        
+        return jobs
