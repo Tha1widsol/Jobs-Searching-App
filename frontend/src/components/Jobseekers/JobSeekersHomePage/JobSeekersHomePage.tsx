3638b0d0d7dd5dd107d6e4a173ff22c005020d93
Kelvin <laikelvin8@gmail.com>
2022-09-19 11:18:36 +0100

    Removed unknown matching score

--- frontend/src/components/Jobseekers/JobSeekersHomePage/JobSeekersHomePage.tsx
+++ frontend/src/components/Jobseekers/JobSeekersHomePage/JobSeekersHomePage.tsx
@@ -54,7 +54,7 @@ export default function JobSeekersHomePage() {
                </KebabMenu>
  
                <Link to = {`/job/${matching.job?.id}`}><h2>{matching.job?.title}</h2></Link>
-               <p style = {{color: 'gray'}}> {matching.score || '??'}% - matching score</p>
+               <p style = {{color: 'gray'}}> {matching.score}% - matching score</p>
              
                <Link to = {`/company/${matching.company?.id}`}><p>{matching.company?.name}</p></Link>
                {matching.job?.salary2 ? <p>{matching.job?.currency}{matching.job?.salary1} - {matching.job?.currency}{matching.job?.salary2} a year </p> : <p>{matching.job?.currency}{matching.job?.salary1} a year</p>} 
