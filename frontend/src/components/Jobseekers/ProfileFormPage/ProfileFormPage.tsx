f253d904ac5f88ef86e77a42c955446453bf7045
Kelvin <laikelvin8@gmail.com>
2022-10-02 12:57:51 +0100

    Added ProfileDetailsForm component

--- frontend/src/components/Jobseekers/ProfileFormPage/ProfileFormPage.tsx
+++ frontend/src/components/Jobseekers/ProfileFormPage/ProfileFormPage.tsx
@@ -10,6 +10,7 @@ import List from '../../Global/Forms/List';
 import axios from 'axios';
 import {token} from '../../Global/features/Auth/user';
 import Popup from '../../Global/Popup/Popup';
+import ProfileDetailsForm from './ProfileDetailsForm/ProfileDetailsForm';
 import ReactScrollableFeed from 'react-scrollable-feed';
 
 export default function ProfileFormPage() {
@@ -19,11 +20,6 @@ export default function ProfileFormPage() {
     const profile = useAppSelector(state => state.profile)
     const [currentTab,setCurrentTab] = useState(1)
     const [errors,setErrors] = useState<Array<string>>([])
-    const [firstName,setFirstName] = useState({value: '', isValid: true, errorMsg: 'First name is invalid'})
-    const [middleName,setMiddleName] = useState({value: '', isValid: true, errorMsg: 'Middle name is invalid'})
-    const [lastName,setLastName] = useState({value: '', isValid: true, errorMsg: 'Last name is invalid'})
-    const [phone,setPhone] = useState({value: '', isValid: true, errorMsg: 'Phone number is invalid'})
-    const [about,setAbout] = useState({value: '', isValid: true, currentLength: Number(profile?.values?.about?.length), maxLength: 250, errorMsg: 'About section needs to have atleast 100 characters'})
     const [skills,setSkills] = useState<ListProps>({value: [], currentVal: '',isEmpty: false, emptyErrorMsg: 'Invalid skill', alreadyExists: false, alreadyExistsMsg: 'Skill already exists',AddedMsg:'Skill added',RemovedMsg: 'Skill removed'})
     const [experience,setExperience] = useState({value: [{title: '', companyName: '',  EmployerName: '', EmployerEmail: '', EmployerPhone: '', description: '', years: 1, isOnGoing: false}], popup: false, currentVal: {title: '', companyName: '', EmployerName: '', EmployerEmail: '', EmployerPhone: '', description: '', years: 1, isOnGoing: false}, isValid: true, currentErrorMsg: '', alreadyExistsMsg: 'Experience already exists'})
     const [education,setEducation] = useState({value: 'No formal education'})
@@ -44,53 +40,8 @@ export default function ProfileFormPage() {
     const validateForm = () => {
         let isValid = true
         let errors : Array<string> = []
-        const letters = /^[A-Za-z]+$/
-        const numbers = /[0-9]/g
 
         switch(currentTab){
-          case 1: 
-            if (!firstName.value.match(letters)){
-                setFirstName(prev => {return {...prev,isValid: false}})
-                errors.push(firstName.errorMsg)
-                isValid = false
-            }
-    
-            else setFirstName(prev => {return {...prev,isValid: true}})
-    
-            if (middleName.value !== '' && !middleName.value?.match(letters)){
-                setMiddleName(prev => {return {...prev,isValid: false}})
-                errors.push(middleName.errorMsg)
-                isValid = false
-            }
-    
-            else setMiddleName(prev => {return {...prev,isValid: true}})
-    
-            if (!lastName.value.match(letters)){
-                setLastName(prev => {return {...prev,isValid: false}})
-                errors.push(lastName.errorMsg)
-                isValid = false
-            }
-    
-            else setLastName(prev => {return {...prev,isValid: true}})
-
-            if (!phone.value.match(numbers) || phone.value.length < 9){
-                setPhone(prev => {return {...prev, isValid: false}})
-                errors.push(phone.errorMsg)
-                isValid = false
-            }
-
-            else setPhone(prev => {return {...prev, isValid: true}})
-
-            if (about.value.length < 100){
-                setAbout(prev => {return {...prev,isValid: false}})
-                errors.push(about.errorMsg)
-                isValid = false
-            }
-    
-            else setAbout(prev => {return {...prev,isValid: true}})
-
-            break
-        
           case 2:
               if (!skills.value.length){
                   setSkills(prev => {return {...prev, isEmpty: true}})
@@ -135,17 +86,10 @@ export default function ProfileFormPage() {
 
         let form = new FormData();
 
-        form.append('firstName',firstName.value)
-        if (middleName.value) form.append('middleName',middleName?.value)
-        form.append('lastName',lastName.value)
-        form.append('phone',phone.value)
-        form.append('about',about.value)
-
+      
         if (cv.value)
            form.append('cv',cv.value,cv.name)
         
-        if (logo.value)
-           form.append('logo',logo.value,logo.name)
         
         form.append('experience', JSON.stringify(experience.value.slice(1)))
         form.append('skills',skills.value.toString())
@@ -218,29 +162,8 @@ export default function ProfileFormPage() {
                 <span className = {`step ${currentTab === 5 ? 'active' : currentTab > 5 ? 'finish' : null}`} onClick = {e => e.currentTarget.className === 'step finish' ? setCurrentTab(5) : null}><p className = 'step-label'>Preferences</p></span>
             </div>
             
-            <form className = 'Form'>
                 <div className = {`tab ${currentTab === 1 ? 'show' : 'hide'}`}>
-                  <h1 className = 'title'>Details</h1> 
-                    <Errors errors = {errors}/>
-
-                    <label htmlFor = 'firstName'><h3>First name:</h3></label>
-                    <input id = 'firstName' value = {firstName.value} className = {!firstName.isValid ? 'inputError' : ''} onChange = {e => setFirstName(prev => {return {...prev, value: e.target.value}})} onKeyUp = {handleFixName} placeholder = 'First name...' autoComplete = 'on' required/>
-
-                    <label htmlFor = 'middleName'><h3>Middle name (Optional):</h3></label>
-                    <input id = 'middleName' value = {middleName.value} className = {!middleName.isValid ? 'inputError' : ''} onChange = {e => setMiddleName(prev => {return {...prev, value: e.target.value}})} placeholder = 'Middle name...' onKeyUp = {handleFixName} autoComplete = 'on'/>
-
-                    <label htmlFor = 'lastName'><h3>Last name:</h3></label>
-                    <input id = 'lastName' value = {lastName.value} className = {!lastName.isValid ? 'inputError' : ''}  onChange = {e => setLastName(prev => {return {...prev, value: e.target.value}})} placeholder = 'Last name...' onKeyUp = {handleFixName} autoComplete = 'on' required/>
-
-                    <label htmlFor = 'phone'><h3>Phone number: (Only provided to employers)</h3></label>
-                    <input id = 'phone' value = {phone.value} type = 'tel' className = {!phone.isValid ? 'inputError' : ''} onChange = {e => setPhone(prev => {return {...prev, value: e.target.value}})} placeholder = 'Phone number...' autoComplete = 'on' maxLength = {15} required/>
-
-                    <label htmlFor = 'about' ><h3>About (Characters remaining: {about.maxLength - about.currentLength}):</h3></label>
-                    <textarea id = 'about' value = {about.value} className = {!about.isValid ? 'inputError' : ''} onChange = {e => setAbout(prev => {return {...prev,currentLength: e.target.value.length, value: e.target.value}})} placeholder = 'Tell us about yourself...' maxLength = {about.maxLength} style = {{height:'100px'}} required/>
-
-                    <label htmlFor = 'logo'><h3>Profile logo (Optional):</h3></label>
-                    <input id = 'logo' type = 'file' accept = 'image/*' autoComplete = 'on' onChange = {e => {if (!e.target.files) return; setLogo({value: e.target.files[0], name: e.target.files[0].name})}}/> 
-
+                      <ProfileDetailsForm popupOff = {() => {}} toggleTab = {() => setCurrentTab(currentTab + 1)} isIsolated = {false}/>
                 </div>
 
                 <div className = {`tab ${currentTab === 2 ? 'show' : 'hide'}`}>
@@ -382,9 +305,8 @@ export default function ProfileFormPage() {
 
                 </div>
                 
-                {currentTab === maxTabs ? <button type = 'button' id = 'submit' onClick = {handleSubmitForm} >Submit</button> : <button type = 'button' className = 'toggleTabBtn' onClick = {validateForm} style = {{float:'right'}}>Next</button>}
+                {currentTab === maxTabs ? <button type = 'button' id = 'submit' onClick = {handleSubmitForm} >Submit</button> : null}
                 <button type = 'button' className = {currentTab > 1 ? 'toggleTabBtn' : 'hide'} onClick = {handleToPrevTab}>Previous</button>
-            </form>
 
         </div>
     )
