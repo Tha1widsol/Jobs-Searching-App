bbb4c3ba77251a1b298f2241f2eb4d6b2b001373
Kelvin <laikelvin8@gmail.com>
2022-10-30 21:48:38 +0000

    Added generic boolean field to Skill model

--- backend/main/models.py
+++ backend/main/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Skill(models.Model):
       name = models.CharField(max_length = 100)
+      generic = models.BooleanField(default = True)
       
       def __str__(self):
           return self.name
