1d48b983adf4f0b0ba90ca114b461addf1c17ae2
Kelvin <laikelvin8@gmail.com>
2022-11-06 22:52:55 +0000

    Added profileProps

---
+++ frontend/src/components/Global/features/Jobseekers/profiles/types/profileProps.ts
@@ -0,0 +1,23 @@
+import { StatusProps } from "../../../../types/status"
+import { UserProps } from "../../../Auth/user"
+
+export interface ProfileProps extends StatusProps{
+    values: {
+        id: number
+        user: UserProps
+        firstName: string
+        lastName: string
+        middleName?: string
+        skills: [{id: 0, name: string}]
+        phone: string
+        logo?: string
+        cv: string
+        education: string
+        industry: string
+        distance: string
+        experience?: string
+        about: string
+        isActive: boolean
+    }
+  
+}
