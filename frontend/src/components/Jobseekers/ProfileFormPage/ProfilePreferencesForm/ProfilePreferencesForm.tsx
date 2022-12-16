308c6cdfe40954c88230c57707bd688f420cdb5c
Kelvin <laikelvin8@gmail.com>
2022-12-16 23:01:18 +0000

    Added request functionality to editPreferences

--- frontend/src/components/Jobseekers/ProfileFormPage/ProfilePreferencesForm/ProfilePreferencesForm.tsx
+++ frontend/src/components/Jobseekers/ProfileFormPage/ProfilePreferencesForm/ProfilePreferencesForm.tsx
@@ -7,13 +7,14 @@ import { FileProps } from '../../../Global/types/forms';
 import { token } from '../../../Global/features/Auth/user';
 import Errors from '../../../Global/messages/Errors';
 import { handleAddSuccessMsg } from '../../../Global/messages/SuccessAlert';
+import { editPreferences } from '../../../Global/features/Jobseekers/profiles/profile';
 
-export default function ProfilePreferencesForm({profile}: {profile: ProfileProps['values']}) {
+export default function ProfilePreferencesForm({profile, isIsolated = false, popupOff}: {profile: ProfileProps['values'], isIsolated?: boolean, popupOff?: () => void} ) {
     const dispatch = useAppDispatch()
     const navigate = useNavigate()
-    const [industry, setIndustry] = useState({value: 'Any'})
-    const [distance, setDistance] = useState({value: 'Any'})
-    const [cv, setCV] = useState<FileProps>({value: '', name:''})
+    const [industry, setIndustry] = useState({value: profile.industry})
+    const [distance, setDistance] = useState({value: profile.distance})
+    const [cv, setCV] = useState<FileProps>({value: profile.cv, name:''})
     const [errors, setErrors] = useState<Array<string>>([])
 
     function handleSubmitForm(e: React.SyntheticEvent){
@@ -31,11 +32,22 @@ export default function ProfilePreferencesForm({profile}: {profile: ProfileProps
         form.append('industry', industry.value)
         form.append('distance', distance.value)
 
-          axios.post('/api/profile',form, requestOptions)
+          axios.put('/api/editPreferences',form, requestOptions)
           .then(response => {
-              if (response.status === 201){
-                  handleAddSuccessMsg('Profile is successfully saved', dispatch)
-                  navigate(`/profile/${profile.user?.id}`)
+              if (response.status === 200){
+                  dispatch(editPreferences({
+                    cv: cv.value,
+                    industry: industry.value,
+                    distance: distance.value
+                  }))
+           
+                if (isIsolated) {
+                  if (popupOff) popupOff()
+                    return
+                }
+                handleAddSuccessMsg('Profile is successfully saved', dispatch)
+                navigate(`/profile/${profile.user?.id}`)
+
               }
               
           })
@@ -47,6 +59,7 @@ export default function ProfilePreferencesForm({profile}: {profile: ProfileProps
 
   return (
     <form onSubmit = {handleSubmitForm}>
+      <Errors errors = {errors}/>
         <h1 className = 'title'>Preferences</h1> 
         <label htmlFor = 'industry'><h3>Industry: (What job industry are you looking to work in ?)</h3></label>
         <select id = 'industry' onChange = {e => setIndustry({value: e.target.value})} value = {industry.value} autoComplete = 'on'>
@@ -66,7 +79,7 @@ export default function ProfilePreferencesForm({profile}: {profile: ProfileProps
             <option value = '20'>20 miles</option>
             <option value = '30'>30 miles</option>
             <option value = '40'>40 miles</option>
-            <option value = '50+'>50 miles</option>
+            <option value = '50+'>50+ miles</option>
         </select>
       <button style = {{float: 'right'}}>Submit</button>
     </form>
